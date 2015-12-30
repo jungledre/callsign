ac12e66c3e6bbd5cec5701c4e95d9a2149db7b0a
jungledre <adrie.k@gmail.com>
2015-12-30 13:08:02 -0800

    tabs to spaces

--- assets/js/components/Header.js
+++ assets/js/components/Header.js
@@ -1,34 +1,34 @@
 var React = require('react');
 var Header = React.createClass({
-	render: function() {
-		return (
-			<nav className="navbar navbar-default" role="navigation">
-				<div className="container">
-					<div className="navbar-header">
-						<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-							<span className="sr-only">Toggle navigation</span>
-							<span className="icon-bar"></span>
-							<span className="icon-bar"></span>
-							<span className="icon-bar"></span>
-						</button>
-						<a className="navbar-brand" onClick={this.props.splash} href='#'>CallSign</a>
-					</div>
+    render: function() {
+        return (
+            <nav className="navbar navbar-default" role="navigation">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" onClick={this.props.splash} href='#'>CallSign</a>
+                    </div>
 
-					<div className="collapse navbar-collapse navbar-ex1-collapse">
-						<ul className="nav navbar-nav">
-							<li><a onClick={this.props.about} href='#'>About</a></li>
-							<li><a onClick={this.props.newGame} href='#'>New Game</a></li>
-							<li><a onClick={this.props.myGames} href='#'>My Games</a></li>
-						</ul>
-						<ul className="nav navbar-nav navbar-right">
-							<li><a onClick={this.props.signUp} href="#">Signup</a></li>
-							<li><a onClick={this.props.login} href="#">Login</a></li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		)
-	}
+                    <div className="collapse navbar-collapse navbar-ex1-collapse">
+                        <ul className="nav navbar-nav">
+                            <li><a onClick={this.props.about} href='#'>About</a></li>
+                            <li><a onClick={this.props.newGame} href='#'>New Game</a></li>
+                            <li><a onClick={this.props.myGames} href='#'>My Games</a></li>
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><a onClick={this.props.signUp} href="#">Signup</a></li>
+                            <li><a onClick={this.props.login} href="#">Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
 })
 
 module.exports = Header;
--- assets/js/components/MyApp.js
+++ assets/js/components/MyApp.js
@@ -10,42 +10,42 @@ var MyGames = require('./MyGames');
 var GamePlay = require('./GamePlay');
 
 var MyApp = React.createClass({
-	getInitialState:function() {
-		return {content:<Splash/>}
-	},
-	//setting click listeners for navbar
-	splashClick:function() {
-		this.setState({content:<Splash/>})
-	},
-	aboutClick:function() {
-		this.setState({content:<About/>})
-	},
-	myGamesClick:function() {
-		this.setState({content:<MyGames userGame={this.userGameClick}/>})
-	},
-	newGameClick:function() {
-		this.setState({content:<NewGame/>})
-	},
-	loginClick:function() {
-		this.setState({content:<Login/>})
-	},
-	signUpClick:function() {
-		this.setState({content:<SignUp/>})
-	},
-	userGameClick:function() {
-		this.setState({content:<GamePlay/>})
-	},
-	render: function() {
-		return (
-			<div>
-				<Header about={this.aboutClick} splash={this.splashClick} myGames={this.myGamesClick} newGame={this.newGameClick} login={this.loginClick} signUp={this.signUpClick} userGame={this.userGameClick}/>
-				<div className='container'>
-					{this.state.content}
-					<Footer/>
-				</div>
-			</div>
-		)
-	}
+    getInitialState:function() {
+        return {content:<Splash/>}
+    },
+    //setting click listeners for navbar
+    splashClick:function() {
+        this.setState({content:<Splash/>})
+    },
+    aboutClick:function() {
+        this.setState({content:<About/>})
+    },
+    myGamesClick:function() {
+        this.setState({content:<MyGames userGame={this.userGameClick}/>})
+    },
+    newGameClick:function() {
+        this.setState({content:<NewGame/>})
+    },
+    loginClick:function() {
+        this.setState({content:<Login/>})
+    },
+    signUpClick:function() {
+        this.setState({content:<SignUp/>})
+    },
+    userGameClick:function() {
+        this.setState({content:<GamePlay/>})
+    },
+    render: function() {
+        return (
+            <div>
+                <Header about={this.aboutClick} splash={this.splashClick} myGames={this.myGamesClick} newGame={this.newGameClick} login={this.loginClick} signUp={this.signUpClick} userGame={this.userGameClick}/>
+                <div className='container'>
+                    {this.state.content}
+                    <Footer/>
+                </div>
+            </div>
+        )
+    }
 })
 
 module.exports = MyApp;
